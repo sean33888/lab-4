7de0945adad98341789f96ae1bc71cf17816ccb5
sean33888 <94458445+sean33888@users.noreply.github.com>
2025-05-09 00:34:31 +0800

    Update counter_la_fir.c

--- testbench/counter_la_fir/counter_la_fir.c
+++ testbench/counter_la_fir/counter_la_fir.c
@@ -127,21 +127,13 @@ void main()
 		}
 	}
 */	
-	int* tmp = fir();
-	reg_mprj_datal = *tmp << 16;
-	reg_mprj_datal = *(tmp+1) << 16;
-	reg_mprj_datal = *(tmp+2) << 16;
-	reg_mprj_datal = *(tmp+3) << 16;
-	reg_mprj_datal = *(tmp+4) << 16;
-	reg_mprj_datal = *(tmp+5) << 16;
-	reg_mprj_datal = *(tmp+6) << 16;
-	reg_mprj_datal = *(tmp+7) << 16;
-	reg_mprj_datal = *(tmp+8) << 16;
-	reg_mprj_datal = *(tmp+9) << 16;
-	reg_mprj_datal = *(tmp+10) << 16;	
-
+	for (int i = 0; i < 3; i++) {
+	    int* tmp = fir();
+	    for (int j = 0; j < 11; j++) {
+	    reg_mprj_datal = *(tmp+j) << 16 + 0x005A0000;
+	    }
+        }
 	//print("\n");
 	//print("Monitor: Test 1 Passed\n\n");	// Makes simulation very long!
-	reg_mprj_datal = 0xAB510000;
 }
 

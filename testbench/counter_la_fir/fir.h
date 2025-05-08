c34d4f1d4ca1470004f68d25791f40679919966a
sean33888 <94458445+sean33888@users.noreply.github.com>
2025-05-09 00:24:03 +0800

    Update fir.h

--- testbench/counter_la_fir/fir.h
+++ testbench/counter_la_fir/fir.h
@@ -3,6 +3,13 @@
 
 #define N 11
 
+#define reg_fir_control        (*(volatile uint32_t*) 0x30000000)
+#define reg_fir_data_length    (*(volatile uint32_t*) 0x30000010)
+#define reg_fir_tap_num        (*(volatile uint32_t*) 0x30000014)
+#define reg_fir_x              (*(volatile uint32_t*) 0x30000040)
+#define reg_fir_y              (*(volatile uint32_t*) 0x30000044)
+#define reg_fir_coeff(i)       (*(volatile uint32_t*) (0x30000080 + (i <<2)))
+
 int taps[N] = {0,-10,-9,23,56,63,56,23,-9,-10,0};
 int inputbuffer[N];
 int inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,11};

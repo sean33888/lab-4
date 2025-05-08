356cd1b51cf06aedb63c75c5a3d64fc96eb1cccc
sean33888 <94458445+sean33888@users.noreply.github.com>
2025-05-09 00:23:02 +0800

    Update fir.c

--- testbench/counter_la_fir/fir.c
+++ testbench/counter_la_fir/fir.c
@@ -1,12 +1,24 @@
 #include "fir.h"
+#include <stdint.h>
 
 void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
 	//initial your fir
+    reg_fir_data_length = data_length;
+    reg_fir_tap_num = N;
+    for(int i = 0; i < N; i++){
+        reg_fir_coeff(i) = taps[i];
+    }
 }
 
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
 	initfir();
 	//write down your fir
+    reg_fir_status = 0x00000001;
+    int reg_fir_x = 0;
+    for (int i=0; i<data_length; i++){
+        reg_fir_x = i+1;
+        outputbuffer[i] = reg_fir_y;
+    }
 	return outputsignal;
 }
 		
